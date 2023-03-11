25d4051aeed374467aaa95ad2c2833c13243e65d
Crowdbotics <engineering@crowdbotics.com>
2023-03-11 00:13:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/snowy_wildflower_de_68699/MainActivity.java
@@ -0,0 +1,15 @@
+package com.snowy_wildflower_de_68699;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "snowy_wildflower_de_68699";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snowy-wildflower-de-68699",
+  url: "https://snowy-wildflower-de-68699.botics.co",
+  api: "https://snowy-wildflower-de-68699.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
